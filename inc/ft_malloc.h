d686cefb34768e94c971d53ebecb5cb754a85e6c
Joris GOUNAND <jgounand@e1r9p16.42.fr>
2018-04-15 22:36:11 +0200

    malloc

--- inc/ft_malloc.h
+++ inc/ft_malloc.h
@@ -6,7 +6,7 @@
 /*   By: jgounand <joris@gounand.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/15 20:50:34 by jgounand          #+#    #+#             */
-/*   Updated: 2018/04/15 21:56:38 by jgounand         ###   ########.fr       */
+/*   Updated: 2018/04/15 22:36:06 by jgounand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,8 @@ typedef struct	s_mem
 	void	*small;
 	size_t	max_small;
 	void	*large;
-	size_t	max_large;
+//	size_t	max_large;
 }				t_mem;
-
-typedef struct	s_addr
-{
-	void	*start;
-	size_t	len;
-}				t_addr;
 
 extern	t_malloc	g_mem;
 
--- src/ft_malloc.c
+++ src/ft_malloc.c
@@ -6,7 +6,7 @@
 /*   By: jgounand <joris@gounand.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/15 20:46:23 by jgounand          #+#    #+#             */
-/*   Updated: 2018/04/15 22:28:16 by jgounand         ###   ########.fr       */
+/*   Updated: 2018/04/15 22:36:04 by jgounand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ t_mem	*init_mem(void)
 {
 	void	*new;
 
+	if (g_mem)
+		return (g_mem);
 	g_mem = mmap(NULL, sizeof(t_mem) + getpagesize() * TINY * 100 +
 			getpagesize() *SMALL * 100,
 			PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
@@ -30,7 +32,8 @@ t_mem	*init_mem(void)
 	g_mem->max_tiny = g_mem->tiny + getpagesize() * TINY * 100;
 	g_mem->small = g_mem->max_tiny + 1;
 	g_mem->max_small = g_mem->small + getpagesize() * SMALL * 100;
-	g_mem->large = NULL;}
+	g_mem->large = NULL;
+}
 
 void *malloc(size_t size)
 {
